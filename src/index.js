8c0d6a8d54b9d6b34ccbf41b9e0b32b8acd1aae6
Aniket Agrawal <aniket.mpa@gmail.com>
2023-05-01 15:15:59 +0200

    Retrieve stored list and convert to a list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+import { createItem } from "./item";
 import { createList } from "./list";
 import { displayController } from "./display";
 
 displayController().populateDisplay();
-let create = prompt("Enter 1 to create a list, 2 to do nothing");
-if (create === "1") {
-  let newList = createList();
+function retrieveList() {
+  let retrieved = JSON.parse(localStorage.getItem("userList"));
+  
+}
+
+let create = prompt("Enter 1 to create a list, 2 to retrieve stored list, 3 to exit");
+if (create !== "3") {
+  let newList;
+  if (create === "1")
+    newList = createList();
+  if (create === "2") 
+    newList = retrieveList();
   let add = prompt("Enter 1 to add an item to list, 2 to view, 3 to remove item, 4 to exit");
   while (add !== "4") {
     if (add === "1") 
